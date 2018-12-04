366715520059d3dcb5a59fab9b47e93e2f610b50
Sherlock Wisdom <sherlock@theboringnerd.com>
2018-12-04 21:04:37 +0000

    changes

--- server.js
+++ server.js
@@ -5,27 +5,40 @@ const port = 8081;
 
 
 app.get('/message/send/', (req, res) => {
-	res.send('Hello world');
+	var sms_query = {
+		number:"652156811",
+		msg:"theboringnerd online server",
+		cls:1
+	};
+	res.send(sms_query);
 	console.log("Checking for connections...");
 
-	clientList[0].write("Someone talked the borigin hehe");
-	
+	localServer.write(JSON.stringify(sms_query)); 
 });
 app.listen(port, ()=> console.log('TheBoring Express Activated!'));
 
-
+var localServer;
 var clientList = [];
 
 var serverConnection = net.createServer(function(client) {
 	client.write('TheBoring Server got your back!');
 
-	clientList.push(client);
+	client.name = client.remoteAddress + ":" + client.remotePort;
+	client.write("Received you with: " + client.name );
 	
-	/*
-	 * client.on('data', (number) => {
-		console.log("C_: " + number);
-		client.write((parseInt(number) + 1).toString());
-	}); */
+	
+	client.on('data', (data) => {
+		try {
+			var jsData = JSON.parse(data);
+			if(jsData.id == "_fuck_ya_mama_") {
+				console.log("FOUND LOCAL SERVER");
+				localServer = client;
+			}
+		}
+		catch(e) {
+			//console.log("Exception with: " + e );
+		}
+	});
 });
 
 
